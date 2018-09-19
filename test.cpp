737973a6932020c95c936cc0cb1feeb30141c779
sdMac <553828127@qq.com>
2018-09-19 09:32:07 +0800

    update str find

--- test.cpp
+++ test.cpp
@@ -60,7 +60,6 @@ int main(int argc, char const *argv[])
     vector<string> powerVec;
     vector< pair<int, string> > vecPair;
     // cin>>str>>p;
-    
     str = "xyabababcba";
     p = "ab";
     // 构建能量石数组
