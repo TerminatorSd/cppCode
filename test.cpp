5f2945b481d7f15d707dcef02ae0030cd7807001
sdMac <553828127@qq.com>
2018-09-20 09:21:38 +0800

    update power stone

--- test.cpp
+++ test.cpp
@@ -64,10 +64,11 @@ int main(int argc, char const *argv[])
     p = "ab";
     // 构建能量石数组
     makePowerVec(p, powerVec);
-    for(int i = 0, len = powerVec.size(); i < len; i++)
-    {
-        cout<<powerVec[i]<<endl;
-    }
+    // 输出能量石组
+    // for(int i = 0, len = powerVec.size(); i < len; i++)
+    // {
+    //     cout<<powerVec[i]<<endl;
+    // }
     // 找出所有能量石的位置，以及能量石的名字
     lenStr = str.size();
     lenP = p.size();
@@ -82,46 +83,46 @@ int main(int argc, char const *argv[])
             if(pos > -1)
             {
                 vecPair.push_back(make_pair(pos, powerVec[i]));
+                // 替换掉已经找出的能量石
+                string temp = "";
+                for(int j = 0, len = powerVec[i].size(); j < len; j++)
+                {
+                    temp += '#';
+                }
+                copyStr.replace(pos, powerVec[i].length(), temp);
             }
-            // 替换掉已经找出的能量石
-            string temp = "";
-            for(int j = 0, len = powerVec[i].length(); j < len; j++)
-            {
-                temp += '#';
-            }
-            copyStr.replace(pos, powerVec[i].length(), temp);
         }
     }
     sort(vecPair.begin(), vecPair.end(), cmp);
-    for(int i = 0, len = vecPair.size(); i < len; i++)
-    {
-        cout<<vecPair[i].first<<","<<vecPair[i].second<<endl;
-    }
-    // 寻找连续的能量石
     // for(int i = 0, len = vecPair.size(); i < len; i++)
     // {
-    //     int tempI = 0;
-    //     tempLen = vecPair[i].second.size();
-    //     for(int j = i; j < len; j++)
-    //     {
-    //         if(vecPair[j].first + vecPair[j].second.size() == vecPair[j + 1].first) 
-    //         {
-    //             tempI += vecPair[j + 1].second.size();
-    //             tempLen += vecPair[j + 1].second.size();
-    //         }
-    //         else
-    //         {
-    //             break;
-    //         }
-    //     }
-    //     tempVec.push_back(tempLen);
-    //     i += tempI - 1;
+    //     cout<<vecPair[i].first<<","<<vecPair[i].second<<endl;
     // }
-    // // 求能量和
-    // for(int i = 0, len = tempVec.size(); i < len; i++)
-    // {
-    //     res += tempVec[i] * tempVec[i];
-    // }
-    // cout<<res<<endl;
+    // 寻找连续的能量石
+    for(int i = 0, len = vecPair.size(); i < len; i++)
+    {
+        int tempI = 0;
+        tempLen = vecPair[i].second.size();
+        for(int j = i; j < len; j++)
+        {
+            if(vecPair[j].first + vecPair[j].second.size() == vecPair[j + 1].first) 
+            {
+                tempI += vecPair[j + 1].second.size();
+                tempLen += vecPair[j + 1].second.size();
+            }
+            else
+            {
+                break;
+            }
+        }
+        tempVec.push_back(tempLen);
+        i += tempI - 1;
+    }
+    // 求能量和
+    for(int i = 0, len = tempVec.size(); i < len; i++)
+    {
+        res += tempVec[i] * tempVec[i];
+    }
+    cout<<res<<endl;
     return 0;
 }
