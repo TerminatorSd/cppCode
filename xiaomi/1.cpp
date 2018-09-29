26a4c9092eade4598d205cd821578e63700b44f7
sdMac <553828127@qq.com>
2018-09-29 10:04:53 +0800

    add xiaomi

---
+++ xiaomi/1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(const set<int> &se1, const set<int> &se2)
+{
+    return *se1.begin() - *se2.begin();
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin>>n;
+    vector<set<int> > vecs;
+    
+    for(int i = 0; i < n; i++)
+    {
+        int j;  
+        cin >> j;
+        vecs[i].insert(j);
+        while (cin.get() != '\n')   { 
+            cin >> j; 
+            vecs[i].insert(i);
+        } 
+
+
+    }
+    sort(vecs.begin(), vecs.end(), cmp);
+
+    vector<int> c;
+    auto it = vecs.begin();
+    it++;
+    auto it1 = vecs.begin();
+    for(; it1 != vecs.end(); it1++)
+    {        
+        set_intersection(it->begin(), it->end(), it1->begin(), it1->end(), back_inserter(c));
+        if(c.size() > 0)
+        {
+            
+        }
+
+    }
+    
+    
+    
+    return 0;
+}
