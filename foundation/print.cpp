9c1556ee9b0c475277382fd95fc6c921c1ef156e
sdMac <553828127@qq.com>
2019-08-15 22:44:16 +0800

    update print

--- foundation/print.cpp
+++ foundation/print.cpp
@@ -22,31 +22,45 @@ typedef std::vector<pii> vpii;
 typedef std::vector<pis> vpis;
 
 template<class T>
-// template<typename P>
-
 // 打印pair类型的vector
-int PrintPairVector(T t) {
+int printPairVector(T t) {
   for(int i = 0; i < t.size(); i++) {
     cout<<t[i].first<<" "<<t[i].second<<endl;
   }
   return 0;
 }
+
+template<class S>
+// 打印数值类型的vector
+int printVector(S s) {
+  for(int i = 0; i < s.size(); i++) {
+    cout<<s[i]<<endl;
+  }
+  return 0;
+}
+
+template<class M>
 // 打印值为pair的map 
-// int PrintPairMap (P p) {
-//   for(int i = 0; i < p.size(); i++) {
-//     cout<<p[i].first<<" "<<p[i].second<<endl;
-//   }
-//   return 0;
-// }
+int printPairMap (M m) {
+  for(int i = 0; i < m.size(); i++) {
+    cout<<m[i].first<<" "<<m[i].second<<endl;
+  }
+  return 0;
+}
 
-// int main() {
-//  vpii pInt;
-//  pInt.push_back(make_pair(1, 2));
-//  PrintPairVector(pInt);
+int main() {
+  vector<int> vs;
+  vs.push_back(1);
+  vs.push_back(2);
+  printVector(vs);
 
-//  vpis pStr;
-//  pStr.push_back(make_pair(1, "asd"));
-//  PrintPairVector(pStr);
-//  return 0;
-// }
+  vpii pInt;
+  pInt.push_back(make_pair(1, 2));
+  printPairVector(pInt);
+
+  // vpis pStr;
+  // pStr.push_back(make_pair(1, "asd"));
+  // printPairVector(pStr);
+ return 0;
+}
  
